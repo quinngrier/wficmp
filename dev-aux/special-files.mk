7d9538261168525352312fcae417b006960aa911
Quinn Grier <quinn.grier@gmail.com>
2016-05-02 21:53:44 -0500

    Add special-files.mk
    The author of this commit has waived all copyright and
    related or neighboring rights to the extent permitted by
    law as described by the CC0 1.0 Universal Public Domain
    Dedication. You should have received a copy of the full
    dedication along with this commit, typically as a file
    named <CC0-1.0.txt>. If not, it may be available at
    <https://creativecommons.org/publicdomain/zero/1.0/>.

---
+++ dev-aux/special-files.mk
@@ -0,0 +1,27 @@
+all:
+
+## begin_variables
+
+GATBPS = 'gatbps'
+
+## end_variables
+
+## begin_rules
+
+.PHONY: all
+
+all:
+	$(GATBPS) '--' 'CC0-1.0.txt'
+	$(GATBPS) '--' 'dev-aux/CC0-1.0-commit.txt'
+
+## end_rules
+
+#
+# The authors of this file have waived all copyright and
+# related or neighboring rights to the extent permitted by
+# law as described by the CC0 1.0 Universal Public Domain
+# Dedication. You should have received a copy of the full
+# dedication along with this file, typically as a file
+# named <CC0-1.0.txt>. If not, it may be available at
+# <https://creativecommons.org/publicdomain/zero/1.0/>.
+#
