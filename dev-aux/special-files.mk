14a4b77737b7c5254ca7cb1810af867606f46f0d
Quinn Grier <quinn.grier@gmail.com>
2016-05-02 23:31:29 -0500

    Add GATBPS_CONFIG_FILE_RULES.am
    The author of this commit has waived all copyright and
    related or neighboring rights to the extent permitted by
    law as described by the CC0 1.0 Universal Public Domain
    Dedication. You should have received a copy of the full
    dedication along with this commit, typically as a file
    named <CC0-1.0.txt>. If not, it may be available at
    <https://creativecommons.org/publicdomain/zero/1.0/>.

--- dev-aux/special-files.mk
+++ dev-aux/special-files.mk
@@ -15,6 +15,7 @@ all:
 	$(GATBPS) '--' 'build-aux/CC0-1.0-AC-COPYRIGHT.ac'
 	$(GATBPS) '--' 'build-aux/CC0-1.0-AM-COPYRIGHT.am'
 	$(GATBPS) '--' 'build-aux/DATE.sh'
+	$(GATBPS) '--' 'build-aux/GATBPS_CONFIG_FILE_RULES.am'
 	$(GATBPS) '--' 'build-aux/VERSION.sh'
 	$(GATBPS) '--' 'dev-aux/CC0-1.0-commit.txt'
 	$(GATBPS) '--' 'm4/GATBPS_CONFIG_FILE.m4'
