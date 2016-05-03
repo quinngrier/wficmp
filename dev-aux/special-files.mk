cecbbcfb92ffeb8a38ea10e3f4c1ae836bc4b5a5
Quinn Grier <quinn.grier@gmail.com>
2016-05-02 23:21:48 -0500

    Add GATBPS_CONFIG_FILE.m4
    The author of this commit has waived all copyright and
    related or neighboring rights to the extent permitted by
    law as described by the CC0 1.0 Universal Public Domain
    Dedication. You should have received a copy of the full
    dedication along with this commit, typically as a file
    named <CC0-1.0.txt>. If not, it may be available at
    <https://creativecommons.org/publicdomain/zero/1.0/>.

--- dev-aux/special-files.mk
+++ dev-aux/special-files.mk
@@ -17,6 +17,7 @@ all:
 	$(GATBPS) '--' 'build-aux/DATE.sh'
 	$(GATBPS) '--' 'build-aux/VERSION.sh'
 	$(GATBPS) '--' 'dev-aux/CC0-1.0-commit.txt'
+	$(GATBPS) '--' 'm4/GATBPS_CONFIG_FILE.m4'
 	$(GATBPS) '--' 'm4/GATBPS_DEFINE_DATE.m4'
 
 ## end_rules
