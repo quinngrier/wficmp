485082518057d6dbbcf79885d203ee155c3e2d9c
Quinn Grier <quinn.grier@gmail.com>
2016-05-03 22:40:30 -0500

    Add CC0-1.0-file.txt
    The author of this commit has waived all copyright and
    related or neighboring rights to the extent permitted by
    law as described by the CC0 1.0 Universal Public Domain
    Dedication. You should have received a copy of the full
    dedication along with this commit, typically as a file
    named <CC0-1.0.txt>. If not, it may be available at
    <https://creativecommons.org/publicdomain/zero/1.0/>.

--- dev-aux/special-files.mk
+++ dev-aux/special-files.mk
@@ -19,6 +19,7 @@ all:
 	$(GATBPS) '--' 'build-aux/VERSION.sh'
 	$(GATBPS) '--' 'build-aux/texinfo.css'
 	$(GATBPS) '--' 'dev-aux/CC0-1.0-commit.txt'
+	$(GATBPS) '--' 'dev-aux/CC0-1.0-file.txt'
 	$(GATBPS) '--' 'm4/GATBPS_CONFIG_FILE.m4'
 	$(GATBPS) '--' 'm4/GATBPS_CONFIG_FILE_SUBST.m4'
 	$(GATBPS) '--' 'm4/GATBPS_DEFINE_DATE.m4'
